9ef6fecaac9b94640c6bb65b2e1dd5b620cdb5de
mseries580 <mseries580@gmail.com>
2026-04-22 23:31:54 +0500

    Update default SESSION_ID in session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1776594125472_1q42rw5tf",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1774101956143_sggjslvpe",
 PORT: process.env.PORT || "8000"
 };
